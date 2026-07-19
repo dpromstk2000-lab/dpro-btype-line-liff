481972e4a0981ce274f9c6003dc6b009987ad393
dpromstk2000-lab <dpromstk2000@gmail.com>
2026-07-19 13:59:44 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+/**
+ * DPRO 就労継続支援B型 LINE
+ * STEP BTYPE-3 public page configuration
+ * This file contains no secret keys.
+ */
+window.DPRO_BTYPE_CONFIG = Object.freeze({
+  API_BASE_URL: "https://dpro-btype-line-api.dpromstk2000.workers.dev",
+  FACILITY_CODE: "dpro_btype_demo",
+  LIFF_ID: "",
+  APP_VERSION: "BTYPE-3-PUBLIC-INQUIRY-20260719",
+});
